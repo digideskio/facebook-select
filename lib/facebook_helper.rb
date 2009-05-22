559320f5060cef433541c14db4dd62562524e9e4
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2009-05-22 18:36:30 +0800

    Escape javascript
    Signed-off-by: Martijn Lafeber <lafeber@gmail.com>

--- lib/facebook_helper.rb
+++ lib/facebook_helper.rb
@@ -3,16 +3,18 @@ module FacebookHelper
     options.symbolize_keys!
     options[:hint] ||= "Begin typing..."
     options[:id]   ||= sanitize_to_id(name).gsub(/_+$/, '')
+    input_id = "#{options[:id]}_input"
+    auto_id  = "#{options[:id]}_auto"
     
     returning(String.new) do |output|
-      output << text_field_tag(name, nil, :id => "#{options[:id]}_input")
-      output << content_tag(:div, :id => "#{options[:id]}_auto", :target => url_for(url), :class => "facebook-auto") do
+      output << text_field_tag(name, nil, :id => input_id)
+      output << content_tag(:div, :id => auto_id, :target => url_for(url), :class => "facebook-auto") do
         content_tag(:div, options[:hint], :class => "default")
         content_tag(:ul, :class => "feed") do
           tags.map { |tag| content_tag(:li, tag, :value => tag) }.join
         end
       end
-      output << javascript_tag("FancyInput.init('#{options[:id]}_input', '#{options[:id]}_auto')")
+      output << javascript_tag("FancyInput.init('#{ escape_javascript(input_id) }', '#{ escape_javascript(auto_id) }')")
     end
   end
 end
