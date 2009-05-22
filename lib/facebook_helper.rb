a4ae1b943df19362ffc7c36fa5f889637f252285
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2009-05-22 18:33:47 +0800

    Add options
    Signed-off-by: Martijn Lafeber <lafeber@gmail.com>

--- lib/facebook_helper.rb
+++ lib/facebook_helper.rb
@@ -1,15 +1,18 @@
 module FacebookHelper
-  def facebook_select_tag(name, url, tags, introtext = "begin typing")
-    id = sanitize_to_id(name).gsub(/_+$/, '')
+  def facebook_select_tag(name, url, tags = [], options = {})
+    options.symbolize_keys!
+    options[:hint] ||= "Begin typing..."
+    options[:id]   ||= sanitize_to_id(name).gsub(/_+$/, '')
+    
     returning(String.new) do |output|
-      output << text_field_tag(name, nil, :id => "#{id}_input")
-      output << content_tag(:div, :id => "#{id}_auto", :target => url_for(url), :class => "facebook-auto") do
-        content_tag(:div, introtext, :class => "default")
+      output << text_field_tag(name, nil, :id => "#{options[:id]}_input")
+      output << content_tag(:div, :id => "#{options[:id]}_auto", :target => url_for(url), :class => "facebook-auto") do
+        content_tag(:div, options[:hint], :class => "default")
         content_tag(:ul, :class => "feed") do
           tags.map { |tag| content_tag(:li, tag, :value => tag) }.join
         end
       end
-      output << javascript_tag("FancyInput.init('#{id}_input', '#{id}_auto')")
+      output << javascript_tag("FancyInput.init('#{options[:id]}_input', '#{options[:id]}_auto')")
     end
   end
 end
