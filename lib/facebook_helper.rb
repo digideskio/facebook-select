e5da783eee4b2a5f94fb7cd315b93dac0883ca60
Sjoerd Andringa <sjoerd.andringa@innovationfactory.nl>
2009-05-22 18:18:37 +0800

    Use url_for for url argument
    Signed-off-by: Martijn Lafeber <lafeber@gmail.com>

--- lib/facebook_helper.rb
+++ lib/facebook_helper.rb
@@ -2,7 +2,7 @@ module FacebookHelper
   def facebook_select_tag(name, url, tags, introtext = "begin typing")
     returning(String.new) do |output|
       output << text_field_tag(name, nil, :id => "#{name}_input")
-      output << content_tag(:div, :id => "#{name}_auto", :target => "#{url}", :class => "facebook-auto") do
+      output << content_tag(:div, :id => "#{name}_auto", :target => url_for(url), :class => "facebook-auto") do
         content_tag(:div, introtext, :class => "default")
         content_tag(:ul, :class => "feed") do
           tags.map { |tag| content_tag(:li, tag, :value => tag) }.join
